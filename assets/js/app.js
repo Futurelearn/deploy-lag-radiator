e59e4e72ec862ff59499e128b8d8ab9f796781b9
Chris Zetter <zetter@gmail.com>
2014-08-21 20:58:58 +0100

    Move helpers to separate file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,4 @@
-function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-    if (pair[0] === variable) {
-      return pair[1];
-    }
-  }
-  return false;
-};
-
-/*
- * JavaScript Pretty Date
- * Copyright (c) 2011 John Resig (ejohn.org)
- * Licensed under the MIT and GPL licenses.
- */
-
-// Takes an ISO time and returns a string representing how
-// long ago the date represents.
-function prettyDate(time){
-  var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
-    diff = (((new Date()).getTime() - date.getTime()) / 1000),
-    day_diff = Math.floor(diff / 86400);
-
-  if ( isNaN(day_diff) || day_diff < 0)
-    return;
-
-  return day_diff == 0 && (
-      diff < 60 && "just now" ||
-      diff < 120 && "1 minute ago" ||
-      diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-      diff < 7200 && "1 hour ago" ||
-      diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-    day_diff == 1 && "Yesterday" ||
-    day_diff < 7 && day_diff + " days ago" ||
-    day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago" ||
-    day_diff < 365 && Math.ceil( day_diff / 31 ) + " months ago" ||
-    '1 Year+';
-
-}
-
 $(document).ready(function() {
-
   var repo_name = getQueryVariable('repo');
   var refresh_rate = (getQueryVariable('refresh') || 60) * 1000;
   var from_tag = getQueryVariable('from');
